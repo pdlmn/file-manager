d3f2b1426e4edbecc92866d8fa47d3dab210aabd
pdlmn <djeinwings@gmail.com>
2022-06-12 02:10:44 +0200

    feat: add cd functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import path from 'path'
+import fs from 'fs/promises'
 import { homedir } from 'os'
 const { cwd, stdin, stdout } = process
 
@@ -12,6 +13,10 @@ const getUsername = (argsArr) => {
   return username.slice(param.length)
 }
 
+const fileExists = async (file) => fs.access(file)
+  .then(() => true)
+  .catch(() => false)
+
 const filename = () => {
   const username = getUsername(process.argv)
   let currentDir = homedir()
@@ -22,13 +27,21 @@ const filename = () => {
     '.exit': (isSigint) => {
       let exitText = `Thank you for using File Manager, ${username}!`
       // adds addition new line for sigint event so it looks prettier
-      if (isSigint) {
+      if (isSigint === true) {
         exitText = '\n' + exitText
       }
       console.log(exitText)
       process.exit()
     },
-    up: () => currentDir = path.resolve(currentDir, '..')
+    up: async () => currentDir = path.resolve(currentDir, '..'),
+    cd: async (args) => {
+      const newDir = path.resolve(currentDir, args[0])
+      if (!await fileExists(newDir)) {
+        console.log('Operation failed')
+        return
+      }
+      currentDir = newDir
+    }
   }
 
   if (!username) {
@@ -39,21 +52,23 @@ const filename = () => {
   console.log(`Welcome to the File Manager, ${username}!`)
   showPwd()
 
-  const doOperation = chunk => {
-    const command = chunk.toString().trim()
+  const doOperation = async (chunk) => {
+    const input = chunk.toString().trim().split(' ')
+    const command = input[0]
+    const args = input.slice(1)
     if (!operations[command]) {
       console.log('Invalid input')
       return
     }
     try {
-      operations[command]()
+      await operations[command](args)
     } catch {
       console.log('Operation failed')
     }
   }
 
-  stdin.on('data', chunk => {
-    doOperation(chunk)
+  stdin.on('data', async (chunk) => {
+    await doOperation(chunk)
     showPwd()
   })
 
